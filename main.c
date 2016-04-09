601c1a744239de3b7de3d15540b1729dfd1cc4c7
Andre Ohuti <ohuti@hotmail.com>
2016-04-09 16:56:15 -0300

    PI

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+double pi(void);
+
+int main(void){
+
+	printf("Valor de PI: %f\n", pi() );
+
+}
+
+double pi(void){ //Uso do método de série infinita para cálculo de pi
+
+	double valor_pi = 0.0;
+	float x, y = 0;
+
+	for(x = 1; x <= 10000001; x += 2)
+		if(x == 1 || y == 4){
+			valor_pi += (4/x);
+			y = 2;
+		} else {
+			valor_pi -= (4/x);
+			y += 2;
+		}
+
+
+	return valor_pi;
+}
